d38fe4437c52dd7a6cc8dbd37aac59e0a5b3be8e
Ryan Hollink <ryan.hollink@hotmail.com>
2023-02-13 00:16:07 +0100

    basic group and command set-up

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
+import click
 
-def main():
-    print("Hello, World!")
+VALID_MODES = click.Choice(["name"], False)
+VALID_NAME_GENERATORS = click.Choice(["project"], False)
+VALID_IDEA_GENERATORS = click.Choice(["game"], False)
+
+@click.group()
+def CLI():
+    pass
+
+@CLI.command('name')
+@click.argument('generator', type=VALID_NAME_GENERATORS)
+def name(generator: str = ""):
+    print(generator)
+
+@CLI.command('idea')
+@click.argument('generator', type=VALID_IDEA_GENERATORS)
+def name(generator: str = ""):
+    print(generator)
 
 if __name__ == "__main__":
-    main()
+    CLI()
